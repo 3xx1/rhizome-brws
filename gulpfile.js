a2802eb6b6bc92747d94fd289479c84f1602262b
Hidekazu Saegusa <hidekazu.saegusa@frogdesign.com>
2016-07-12 00:19:07 -0700

    adding vendors and killing browserify tasks

--- gulpfile.js
+++ gulpfile.js
@@ -29,12 +29,20 @@ var copyLocations = [
     dest: './public/image'
   },
   {
-    src: ['./src/index.html', './src/login.html'],
+    src: './src/index.html',
     dest: './public'
   },
   {
     src: './node_modules/bootstrap/dist/css/bootstrap.css',
     dest: './public/css'
+  },
+  {
+    src: './src/vendor/**/*.*',
+    dest: './public/vendor'
+  },
+  {
+    src: './src/js/sketches/**/*.*',
+    dest: './public/sketches'
   }
 ];
 
@@ -72,50 +80,50 @@ gulp.task('copy', function(cb) {
 });
 
 /* BROWSERIFY+WATCHIFY */
-var bundler = browserify({
-  basedir: './src/js/',
-  entries: ['index.js'],
-  debug: true
-}).transform("babelify", {
-  presets: ["es2015"]
-});
-
-gulp.task('prod-js', function() {
-  return bundler.bundle()
-  .pipe(source('bundle.js'))
-  .pipe(buffer())
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('prod-sass', function() {
-  gulp.src('src/sass/**/*.scss')
-      .pipe(plumber())
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(cleancss())
-      .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('watchify', function() {
-  var watcher = watchify(bundler);
-  return watcher
-  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-  .on('update', function() {
-    watcher.bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/js'))
-    .pipe(connect.reload());
-
-    gutil.log('Updated Javascript sources');
-  })
-  .bundle()
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest('./public/js'));
-});
+// var bundler = browserify({
+//   basedir: './src/js/',
+//   entries: ['index.js'],
+//   debug: true
+// }).transform("babelify", {
+//   presets: ["es2015"]
+// });
+// 
+// gulp.task('prod-js', function() {
+//   return bundler.bundle()
+//   .pipe(source('bundle.js'))
+//   .pipe(buffer())
+//   .pipe(uglify({mangle: false}))
+//   .pipe(gulp.dest('./public/js'));
+// });
+// 
+// gulp.task('prod-sass', function() {
+//   gulp.src('src/sass/**/*.scss')
+//       .pipe(plumber())
+//       .pipe(sass())
+//       .pipe(autoprefixer())
+//       .pipe(cleancss())
+//       .pipe(gulp.dest('./public/css'));
+// });
+// 
+// gulp.task('watchify', function() {
+//   var watcher = watchify(bundler);
+//   return watcher
+//   .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+//   .on('update', function() {
+//     watcher.bundle()
+//     .pipe(source('bundle.js'))
+//     .pipe(buffer())
+//     .pipe(sourcemaps.init({loadMaps: true}))
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest('./public/js'))
+//     .pipe(connect.reload());
+// 
+//     gutil.log('Updated Javascript sources');
+//   })
+//   .bundle()
+//   .pipe(source('bundle.js'))
+//   .pipe(gulp.dest('./public/js'));
+// });
 
 /* SASS */
 gulp.task('sass', function() {
@@ -148,12 +156,7 @@ gulp.task('dev', ['clean'], function() {
   gulp.start('copy');
   gulp.start('sass');
   gulp.start('sass:watch');
-  gulp.start('watchify');
+//   gulp.start('watchify');
   gulp.start('server');
 });
 
-gulp.task('build', ['clean'], function() {
-  gulp.start('copy');
-  gulp.start('prod-sass');
-  gulp.start('prod-js');
-});
